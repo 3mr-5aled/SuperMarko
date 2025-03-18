abb9171893ff9a156947b8eb79df9731e03026bc
Amr Khaled <3mr5aled.dev@gmail.com>
2025-03-18 23:57:29 +0200

    edit file header

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "edit.h"
 using namespace std;
+
+struct Product
+{
+    /* data */
+};
 
 int main()
 {
     cout << "Welcome to SUPERMARKO" << endl;
-
+    edit();
     return 0;
 }
